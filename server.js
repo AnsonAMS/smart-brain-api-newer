2175e0188da8dae3553fbacd2ca6106ca65a01ca
AnsonAMS <anson_tohata@hotmail.com>
2021-03-24 20:23:54 +0100

    Update host address database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-opaque-52724',
       user : 'anson',
       password : '',
       database : 'smart-brain'
