b1d088078d8b80021d4df0677889a8cfe9b1c256
AnsonAMS <anson_tohata@hotmail.com>
2021-03-24 20:29:29 +0100

    Update hosting info database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-opaque-52724',
-      user : 'anson',
-      password : '',
-      database : 'smart-brain'
+      host : 'process.env.DATABASE_URL',
+      ssl: true,
     }
   });
 
